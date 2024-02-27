9831eb19739e317cdd5712f9f92fe4fb2afa3452
shin-SeungHun <sng7387@gmail.com>
2024-02-27 22:10:39 +0900

    feat: go_router에 의존성 주입

--- lib/router/routes.dart
+++ lib/router/routes.dart
@@ -1,6 +1,12 @@
-import 'package:flutter_todo_list_app/ui/todo_add_screen.dart';
-import 'package:flutter_todo_list_app/ui/todo_list_screen.dart';
-import 'package:flutter_todo_list_app/ui/todo_view_model.dart';
+import 'package:flutter_todo_list_app/data/data_source/todo_db.dart';
+import 'package:flutter_todo_list_app/data/model/todo_model.dart';
+import 'package:flutter_todo_list_app/data/repository/todo_repository_impl.dart';
+import 'package:flutter_todo_list_app/ui/insert/todo_insert_screen.dart';
+import 'package:flutter_todo_list_app/ui/insert/todo_insert_view_model.dart';
+import 'package:flutter_todo_list_app/ui/list/todo_list_screen.dart';
+import 'package:flutter_todo_list_app/ui/list/todo_list_view_model.dart';
+import 'package:flutter_todo_list_app/ui/update/todo_update_screen.dart';
+import 'package:flutter_todo_list_app/ui/update/todo_update_view_model.dart';
 import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
@@ -10,16 +16,25 @@ final router = GoRouter(initialLocation: '/', routes: [
       path: '/',
       builder: (context, state) {
         return ChangeNotifierProvider(
-          create: (context) => GetIt.instance<TodoViewModel>(),
+          create: (context) => GetIt.instance<TodoListViewModel>(),
           child: const TodoListScreen(),
         );
       }),
   GoRoute(
-      path: '/add',
+      path: '/insert',
       builder: (context, state) {
         return ChangeNotifierProvider(
-          create: (context) => GetIt.instance<TodoViewModel>(),
-          child: const TodoAddScreen(),
+          create: (context) => GetIt.instance<TodoInsertViewModel>(),
+          child: const TodoInsertScreen(),
+        );
+      }),
+  GoRoute(
+      path: '/update',
+      builder: (context, state) {
+        final TodoModel todo = state.extra as TodoModel;
+        return ChangeNotifierProvider(
+          create: (context) => TodoUpdateViewModel(todo: todo, repository: GetIt.instance<TodoRepositoryImpl>()),
+          child: const TodoUpdateScreen(),
         );
       }),
 ]);
