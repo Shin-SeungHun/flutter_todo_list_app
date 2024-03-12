1196c8f78bbb92d427480749eb0e91e795c9d5f3
shin-SeungHun <sng7387@gmail.com>
2024-03-12 17:04:52 +0900

    feat: hive db 세팅

--- lib/data/data_source/todo_db.dart
+++ lib/data/data_source/todo_db.dart
@@ -1,65 +1,59 @@
 import 'package:flutter_todo_list_app/data/model/todo_model.dart';
-import 'package:sqflite/sqflite.dart';
+import 'package:hive_flutter/adapters.dart';
 
 class TodoDb {
-  Database db;
+  late final Box<TodoModel> db;
 
   TodoDb({
     required this.db,
   });
 
+  Future<void> init() async {
+    await Hive.initFlutter();
+    Hive.registerAdapter(TodoModelAdapter());
+    db = await Hive.openBox<TodoModel>('todoList.db');
+  }
+
   /// todo list 목록
   Future<List<TodoModel>> getTodoList() async {
-    final maps = await db.query('todo');
-    return maps.map((e) => TodoModel.fromJson(e)).toList();
+    // 모든 항목을 가져와서 TodoModel로 변환한 후 리스트로 반환합니다.
+    final List<TodoModel> todoList = db.values.map((e) => TodoModel.fromJson(e as Map<String, dynamic>)).toList();
+
+    return todoList;
   }
 
   /// todo list 항목 id로 조회
-  Future<TodoModel?> getTodoListById({required int id}) async {
-    final List<Map<String, Object?>> maps = await db.query(
-      'todo',
-      where: 'id = ?',
-      whereArgs: [id],
-    );
+Future<TodoModel?> getTodoListById({required int id}) async {
+  final todo = db.get(id);
 
-    if (maps!.isNotEmpty) {
-      return TodoModel.fromJson(maps.first);
-    }
-    return null;
-  }
+  return todo;
+}
 
   /// todo list 추가
   Future<void> insertTodoList({required TodoModel todo}) async {
-    await db.insert('todo', todo.toJson());
+    await db.put(todo.id, todo);
   }
 
   /// todo list 업데이트
   Future<void> updateTodoList({required TodoModel todo}) async {
-    await db.update(
-      'todo',
-      todo.toJson(),
-      where: 'id = ?',
-      whereArgs: [todo.id],
-    );
+    if (todo.id != null) {
+      await db.putAt(todo.id!, todo);
+    }
   }
 
   /// todo list 삭제
   Future<void> deleteTodoList({required TodoModel todo}) async {
-    await db.delete(
-      'todo',
-      where: 'id = ?',
-      whereArgs: [todo.id],
-    );
+    await db.delete(todo.id);
   }
 
-  Future<void> checkTodoList({required TodoModel todo}) async {
-    final int isDoneValue = todo.isDone ? 1 : 0;
-    await db.update(
-      'todo',
-      {'isDone': isDoneValue},
-      where: 'id = ?',
-      whereArgs: [todo.id],
-    );
-    print('Todo checked: ${todo.isDone}');
-  }
+// Future<void> checkTodoList({required TodoModel todo}) async {
+//   final int isDoneValue = todo.isDone ? 1 : 0;
+//   await db.update(
+//     'todo',
+//     {'isDone': isDoneValue},
+//     where: 'id = ?',
+//     whereArgs: [todo.id],
+//   );
+//   print('Todo checked: ${todo.isDone}');
+// }
 }
