7f214002ee7647e96774bf85bbc27e70645f52a1
shin-SeungHun <sng7387@gmail.com>
2024-03-12 18:31:22 +0900

    refactor: main에서 hive, adapter 초기화

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo_list_app/data/model/todo_model.dart';
 import 'package:flutter_todo_list_app/di/di_set_up.dart';
 import 'package:flutter_todo_list_app/router/routes.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await diSetup();
+  await init();
   runApp(const MyApp());
+}
+
+Future<void> init()async{
+  await Hive.initFlutter();
+  Hive.registerAdapter(TodoModelAdapter());
+  await diSetup();
 }
 
 class MyApp extends StatelessWidget {
