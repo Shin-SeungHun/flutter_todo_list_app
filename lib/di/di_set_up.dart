7ed6944f4cef7c362e9ab6bc687c7495fa9d42de
shin-SeungHun <sng7387@gmail.com>
2024-03-12 18:31:40 +0900

    refactor: di에서 db 생성

--- lib/di/di_set_up.dart
+++ lib/di/di_set_up.dart
@@ -1,47 +1,26 @@
 import 'package:flutter_todo_list_app/data/data_source/todo_db.dart';
+import 'package:flutter_todo_list_app/data/model/todo_model.dart';
 import 'package:flutter_todo_list_app/data/repository/todo_repository_impl.dart';
 import 'package:flutter_todo_list_app/ui/insert/todo_insert_view_model.dart';
 import 'package:flutter_todo_list_app/ui/list/todo_list_view_model.dart';
 import 'package:get_it/get_it.dart';
-import 'package:sqflite/sqflite.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 final GetIt getIt = GetIt.instance;
 
 diSetup() async {
-  final db = await _openDatabaseWithDelay();
+  late Box<TodoModel> db;
+  db = await Hive.openBox<TodoModel>('todoList.db');
+  print('All items in todoList box:');
+  for (var key in db.keys) {
+    var item = db.get(key);
+    print('item ${item?.isDone.runtimeType}');
+    print('todoList ${db.keys};');
+  }
+  TodoDb todoDb = TodoDb(db: db);
 
-  final todoDb = TodoDb(db: db); // 데이터베이스 인스턴스 생성
   getIt.registerSingleton<TodoRepositoryImpl>(TodoRepositoryImpl(db: todoDb)); // TodoRepositoryImpl 등록
 
   getIt.registerFactory<TodoListViewModel>(() => TodoListViewModel(repository: getIt<TodoRepositoryImpl>()));
   getIt.registerFactory<TodoInsertViewModel>(() => TodoInsertViewModel(repository: getIt<TodoRepositoryImpl>()));
-}
-
-Future<Database> _openDatabaseWithDelay() async {
-  // 데이터베이스를 열 때 지연을 추가하여 초기화될 때까지 기다립니다.
-  await Future.delayed(const Duration(seconds: 1));
-  return await openDatabase(
-    'todoList.db',
-    version: 1,
-    onCreate: (db, version) async {
-      await db.execute(
-          'CREATE TABLE todo (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, dateTime TEXT, isDone INTEGER)');
-    },
-    onOpen: (db) async {
-      // 데이터베이스가 열릴 때 호출되는 콜백에서 테이블이 없으면 테이블 생성
-      final isTableExists = await _isTableExists(db, 'todo');
-      if (!isTableExists) {
-        await db.execute(
-          'CREATE TABLE todo (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, dateTime TEXT, isDone INTEGER)',
-        );
-      }
-    },
-  );
-}
-
-Future<bool> _isTableExists(Database db, String tableName) async {
-  final result = await db.rawQuery(
-    "SELECT * FROM sqlite_master WHERE type = 'table' AND name = '$tableName'",
-  );
-  return result.isNotEmpty;
 }
